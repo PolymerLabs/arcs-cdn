56b866bbc58af0066fead4a89168618b485f0c69
Bernhard A. M. Seefeld <seefeld@gmail.com>
2017-10-04 18:11:29 -0700

    add license headers

--- dev.1/app-shell/lib/firebase.js
+++ dev.1/app-shell/lib/firebase.js
@@ -1,3 +1,13 @@
+/**
+ * @license
+ * Copyright (c) 2017 Google Inc. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * Code distributed by Google as part of this project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+
 (function(scope) {
 
     let version = typeof Arcs === 'undefined' || !Arcs.version ? '/' : Arcs.version.replace(/\./g, '_');
--- dev.1/app-shell/lib/storage-tools.js
+++ dev.1/app-shell/lib/storage-tools.js
@@ -1,3 +1,13 @@
+/**
+ * @license
+ * Copyright (c) 2017 Google Inc. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * Code distributed by Google as part of this project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+
 ((scope) => {
 
 const pre = [`%cStorageTools`, `background: #280680; color: white; padding: 1px 6px 2px 7px; border-radius: 6px;`];
