baab3fec6f7d22450cd3cc2464d3ee7bac32041b
Scott J Miles <sjmiles@google.com>
2017-10-06 16:07:15 -0700

    rebuild ArcsLib

--- dev/source/ArcsLib.js
+++ dev/source/ArcsLib.js
@@ -1,4 +1,4 @@
-OB/**
+/**
  * @license
  * Copyright (c) 2017 Google Inc. All rights reserved.
  * This code may only be used under the BSD style license found at
@@ -11,12 +11,12 @@ OB/**
 let Arcs = {
   version: '0.2',
   Arc: require(`../../../arcs/runtime/arc.js`),
-  BrowserLoader: require(`./browser-cdn-loader.js`),
   Description: require('../../../arcs/runtime/description.js'),
   Manifest: require("../../../arcs/runtime/manifest.js"),
   Planner: require(`../../../arcs/runtime/planner.js`),
-  SlotComposer: require(`../../../arcs/runtime/slot-composer.js`)
-  Type: require('../../../arcs/runtime/type.js')
+  SlotComposer: require(`../../../arcs/runtime/slot-composer.js`),
+  Type: require('../../../arcs/runtime/type.js'),
+  BrowserLoader: require(`./browser-cdn-loader.js`)
 };
 
 window.Arcs = Arcs;
