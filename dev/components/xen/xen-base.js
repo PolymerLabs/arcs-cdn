139021afa1c3500fb95b55cc530a04410922bba3
Scott J Miles <sjmiles@google.com>
2017-10-18 13:30:47 -0700

    xen-base creates shadow-roots now as default

--- dev/components/xen/xen-base.js
+++ dev/components/xen/xen-base.js
@@ -3,13 +3,25 @@ class XenBase extends XenState(XenElement) {
     // TODO(sjmiles): null check module and template
     return this.constructor.module.querySelector('template');
   }
+  get host() {
+    return this.shadowRoot || this.attachShadow({mode: `open`});
+  }
   _doMount() {
     this._stamp();
+    this._invalidate();
   }
   _stamp() {
-    this._dom = Xen.stamp(this.template).events(this).appendTo(this);
+    this._dom = Xen.stamp(this.template).events(this).appendTo(this.host);
   }
   _update(props, state) {
-    this._dom.set(this._render(props, state));
+    let model = this._render(props, state);
+    if (Array.isArray(model)) {
+      model = model.reduce((sum, value) => Object.assign(sum, value), Object.create(null));
+    }
+    this._dom.set(model);
+  }
+  _render(props, state) {
   }
 }
+XenBase.logFactory = (preamble, color, log='log') => console[log].bind(console, `%c${preamble}`, `background: ${color}; color: white; padding: 1px 6px 2px 7px; border-radius: 6px;`);
+XenBase.log = XenBase.logFactory('XenBase', '#673AB7');
