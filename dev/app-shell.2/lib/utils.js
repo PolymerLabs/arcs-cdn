b5964f9bae862930b082bafc8c3442606dcd116c
Scott J Miles <sjmiles@google.com>
2017-10-23 19:12:15 -0700

    app-shell refactoring

--- dev/app-shell.2/lib/utils.js
+++ dev/app-shell.2/lib/utils.js
@@ -72,10 +72,24 @@ let utils = {
   randomId() {
     return Date.now().toString(36).substr(2) + Math.random().toString(36).substr(2);
   },
+  randomName() {
+    const adjectives = ["adamant", "adroit", "amatory", "animistic", "antic", "arcadian", "baleful", "bellicose", "bilious", "boorish", "calamitous", "caustic", "cerulean", "comely", "concomitant", "contumacious", "corpulent", "crapulous", "defamatory", "didactic", "dilatory", "dowdy", "efficacious", "effulgent", "egregious", "endemic", "equanimous", "fastidious", "feckless", "friable", "fulsome", "garrulous", "guileless", "gustatory", "heuristic", "histrionic", "hubristic", "incendiary", "insidious", "insolent", "intransigent", "inveterate", "invidious", "irksome", "jejune", "jocular", "judicious", "lachrymose", "limpid", "loquacious", "luminous", "mannered", "mendacious", "meretricious", "minatory", "mordant", "munificent", "nefarious", "noxious", "obtuse", "parsimonious", "pendulous", "pernicious", "pervasive", "petulant", "platitudinous", "precipitate", "propitious", "puckish", "querulous", "quiescent", "rebarbative", "recalcitrant", "redolent", "rhadamanthine", "risible", "ruminative", "sagacious", "salubrious", "sartorial", "sclerotic", "serpentine", "spasmodic", "strident", "taciturn", "tenacious", "tremulous", "trenchant", "turbulent", "turgid", "ubiquitous", "uxorious", "verdant", "voluble", "voracious", "wheedling", "withering", "zealous"];
+    const nouns = ["ninja", "chair", "pancake", "statue", "unicorn", "rainbows", "laser", "senor", "bunny", "captain", "nibblets", "cupcake", "carrot", "gnomes", "glitter", "potato", "salad", "toejam", "curtains", "beets", "toilet", "exorcism", "stick figures", "mermaid eggs", "sea barnacles", "dragons", "jellybeans", "snakes", "dolls", "bushes", "cookies", "apples", "ice cream", "ukulele", "kazoo", "banjo", "opera singer", "circus", "trampoline", "carousel", "carnival", "locomotive", "hot air balloon", "praying mantis", "animator", "artisan", "artist", "colorist", "inker", "coppersmith", "director", "designer", "flatter", "stylist", "leadman", "limner", "make-up artist", "model", "musician", "penciller", "producer", "scenographer", "set decorator", "silversmith", "teacher", "auto mechanic", "beader", "bobbin boy", "clerk of the chapel", "filling station attendant", "foreman", "maintenance engineering", "mechanic", "miller", "moldmaker", "panel beater", "patternmaker", "plant operator", "plumber", "sawfiler", "shop foreman", "soaper", "stationary engineer", "wheelwright", "woodworkers"];
+    let rl = list => list[Math.floor(Math.random()*list.length)];
+    return `${rl(adjectives)}-${rl(nouns)}`.replace(/ /g, '-');
+  },
   setUrlParam(name, value) {
     let url = new URL(document.location.href);
     url.searchParams.set(name, value);
     window.history.replaceState({}, "", decodeURIComponent(url.href));
+  },
+  describeArc(arc) {
+    let combinedSuggestion = Arcs.Description.getSuggestion(arc._activeRecipe, arc, null);
+    if (!combinedSuggestion) {
+      combinedSuggestion = arc._fakeName = arc._fakeName || Arcs.utils.randomName();
+    }
+    let tags = Object.keys(arc._tags).filter(t => ['#nosync','#arcmetadata','#identity','#identities'].indexOf(t) < 0);
+    return `${tags.join(", ")}${tags.length ? ' - ' : ''}${combinedSuggestion}`;
   }
 };
 
