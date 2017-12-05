b92a9a5dd1f9610783233c43910509530ed4ab91
Matt Small <mbsmall@google.com>
2017-12-05 11:35:56 -0800

    small fixes for the webdriver test - consolidate test logic in the test spec (deleting the old standalone test) - organize webdriver dependencies into dev - ignore errorShots

--- dev/test/specs/starter-test.js
+++ dev/test/specs/starter-test.js
@@ -12,9 +12,22 @@ let assert = require('assert');
 
 describe('Test a new arc', function() {
   it('Create an arc', function() {
-    browser.url('https://polymerlabs.github.io/arcs-cdn/dev/apps/web/');
-    // note: to drop into debug mode with a REPL
-    // browser.debug()
+    // TODO(smalls) should this create a user on the fly?
+    browser.url('https://polymerlabs.github.io/arcs-cdn/dev/apps/web/?user=-L-YGQo_7f3izwPg6RBn');
     assert.equal('Arcs', browser.getTitle());
+
+    // create a new arc, switch to that tab (toggling back to the first tab to
+    // reset the webdriver window state).
+    browser.waitForVisible('div[title="New Arc"]');
+    browser.click('div[title="New Arc"]');
+    browser.switchTab(browser.windowHandles().value[0]);
+    browser.switchTab(browser.windowHandles().value[1]);
+
+    // check out our structure - these are working
+    browser.waitForVisible('<app-main>');
+    browser.waitForVisible('<footer>');
+
+    // note: to drop into debug mode with a REPL
+    browser.debug()
   });
 });
