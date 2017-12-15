405903293ccc80dca0676ef5c3a1ed1daa40ab14
Matt Small <mbsmall@google.com>
2017-12-15 09:26:44 -0800

    straight copy of v0.2.1 arcs extension to dev Note that this isn't working - it's a straight copy so it still needs to be updated to work with the new app shell. Also add the unit tests to the dev index.test.html for automation

---
+++ dev/apps/chrome-extension/data-processing.js
@@ -0,0 +1,45 @@
+// Copyright (c) 2017 Google Inc. All rights reserved.
+// This code may only be used under the BSD style license found at
+// http://polymer.github.io/LICENSE.txt
+// Code distributed by Google as part of this project is also
+// subject to an additional IP rights grant found at
+// http://polymer.github.io/PATENTS.txt
+
+/**
+ * Reduce the deeply nested structure of url=>entities-of-many-types to a
+ * flatter, combined form of type=>entities.
+ *
+ * For example, the input would be of the form
+ * {'http://g.co': [{@type: 'typeA', 'a': 1}},
+ * the output would be
+ * {typeA: [{@type: 'typeA', 'a': 1}]}.
+ */
+function flatten(entities) {
+  return Object.entries(entities).reduce((accumulator, [key, value]) => {
+    value.forEach(entry => {
+      let type = entry['@type'];
+      accumulator[type]
+        ? accumulator[type].push(entry)
+        : (accumulator[type] = [entry]);
+    });
+    return accumulator;
+  }, new Object());
+}
+
+/**
+ * Removes duplicate entries. Expects the input to match the output format of
+ * #flatten().
+ */
+function deduplicate(entities) {
+  return Object.entries(entities).reduce((accumulator, [key, values]) => {
+    accumulator[key] = values.reduce((accumulator, value) => {
+      let isIncluded = accumulator.reduce(
+        (a, av) => _.isEqual(av, value) || a,
+        false
+      );
+      isIncluded || accumulator.push(value);
+      return accumulator;
+    }, []);
+    return accumulator;
+  }, new Object());
+}
---
+++ dev/apps/chrome-extension/test/data-processing-test.js
@@ -0,0 +1,77 @@
+// Copyright (c) 2017 Google Inc. All rights reserved.
+// This code may only be used under the BSD style license found at
+// http://polymer.github.io/LICENSE.txt
+// Code distributed by Google as part of this project is also
+// subject to an additional IP rights grant found at
+// http://polymer.github.io/PATENTS.txt
+
+var assert = chai.assert;
+
+afterEach(function() {
+  target.innerHTML = '';
+});
+
+describe('ChromeExtensionDataProcessing', function() {
+  describe('#flatten()', function() {
+    it('should flatten & combine single datatype', function() {
+      let sample = {
+        'http://my/great/site': [{ '@type': 'TypeA', name: 'TypeA_MGS' }],
+        'http://my/terrible/site': [{ '@type': 'TypeA', name: 'TypeA_MTS' }]
+      };
+      let expected = {
+        TypeA: [
+          { '@type': 'TypeA', name: 'TypeA_MGS' },
+          { '@type': 'TypeA', name: 'TypeA_MTS' }
+        ]
+      };
+
+      let result = flatten(sample);
+      assert.deepEqual(result, expected);
+    });
+    it('should flatten & combine datatypes', function() {
+      let sample = {
+        'http://my/great/site': [
+          { '@type': 'TypeA', name: 'TypeA_MGS' },
+          { '@type': 'TypeB', name: 'TypeB_MGS' }
+        ],
+        'http://my/terrible/site': [
+          { '@type': 'TypeA', name: 'TypeA_MTS' },
+          { '@type': 'TypeB', name: 'TypeB_MTS' }
+        ]
+      };
+      let expected = {
+        TypeA: [
+          { '@type': 'TypeA', name: 'TypeA_MGS' },
+          { '@type': 'TypeA', name: 'TypeA_MTS' }
+        ],
+        TypeB: [
+          { '@type': 'TypeB', name: 'TypeB_MGS' },
+          { '@type': 'TypeB', name: 'TypeB_MTS' }
+        ]
+      };
+
+      let result = flatten(sample);
+      assert.deepEqual(result, expected);
+    });
+  });
+  describe('#deduplicate()', function() {
+    it('should deduplicate', function() {
+      let sample = {
+        TypeA: [
+          { '@type': 'TypeA', name: 'same' },
+          { '@type': 'TypeA', name: 'same' },
+          { '@type': 'TypeA', name: 'different' }
+        ]
+      };
+      let expected = {
+        TypeA: [
+          { '@type': 'TypeA', name: 'same' },
+          { '@type': 'TypeA', name: 'different' }
+        ]
+      };
+
+      let result = deduplicate(sample);
+      assert.deepEqual(result, expected);
+    });
+  });
+});
