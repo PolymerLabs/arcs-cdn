d7a995c0d3de554fe817ef219d79609ffe3d103c
Matt Small <mbsmall@google.com>
2017-12-19 13:52:21 -0800

    in-progress on retrieving a suggestion via text This path runs into issues with async execution; it may be time to back out and try another fork.

--- dev/test/specs/starter-test.js
+++ dev/test/specs/starter-test.js
@@ -14,14 +14,18 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function queryElements(selectors) {
+  return browser.execute(function(selectors) {
+    return pierceShadows(selectors);
+  }, selectors);
+}
+
 /** wait for the dancing dots to stop. */
 async function waitForStillness() {
   let dotsStopped = false;
   for (let i=0; i < 10; i++) {
-    var result = await browser.execute(function() {
-      let dots = pierceShadows(['arc-footer', 'x-toast[app-footer]', 'dancing-dots']);
-      return dots.getAttribute('animate');
-    });
+    var element = await queryElements(['arc-footer', 'x-toast[app-footer]', 'dancing-dots']);
+    var result = await browser.elementIdAttribute(element.value.ELEMENT, 'animate');
 
     if (result.value==null) {
       dotsStopped = true;
@@ -48,13 +52,15 @@ function loadSeleniumUtils(cdnBranch) {
 
 describe('test a new arc', function() {
   it('creates an arc', async function() {
-    // TODO(smalls) should this create a user on the fly?
 
     // TODO(smalls) need to spin up a server for this
     let cdnBranch = 'http://localhost:8000/arcs-cdn/dev';
 
+    // TODO(smalls) should we create a user on the fly?
     browser.url(`${cdnBranch}/apps/web/?user=-L-YGQo_7f3izwPg6RBn`);
+
     assert.equal('Arcs', browser.getTitle());
+    await browser.debug();
 
     // create a new arc, switch to that tab (toggling back to the first tab to
     // reset the webdriver window state).
@@ -63,14 +69,56 @@ describe('test a new arc', function() {
     browser.switchTab(browser.windowHandles().value[0]);
     browser.switchTab(browser.windowHandles().value[1]);
 
-    // check out our structure - these are working
+    // wait for the page to load a bit, init the test harness for this page
     browser.waitForVisible('<app-main>');
     browser.waitForVisible('<footer>');
-
-    // init our test harness, wait for the dancing dots to stop
     loadSeleniumUtils(cdnBranch);
+
+    // check out some basic structure relative to the app footer
+    let footerPath = ['arc-footer', 'x-toast[app-footer]'];
+    assert.ok(queryElements(footerPath.slice(0, 1)).value);
+    assert.ok(queryElements(footerPath).value);
+
+    // wait for the dancing dots to stop
     await waitForStillness();
 
+    let magnifier = await queryElements(footerPath.concat(['div[search]', 'i']));
+    browser.elementIdClick(magnifier.value.ELEMENT);
+
+    let suggestionsRoot = await queryElements(footerPath.concat(['suggestions-element']));
+    let suggestionsDiv = await queryElements(footerPath.concat(['suggestions-element', 'div']));
+    let allSuggestions = await browser.elementIdElements(
+        suggestionsDiv.value.ELEMENT, 'suggest');
+    let allSuggestionIds = allSuggestions
+      .value
+      .map(value => {
+        return {
+          id: value.ELEMENT, textLater: browser.elementIdText(value.ELEMENT)
+        };
+      });/*.reduce((accumulator, currentValue) => {
+        accumulator[currentValue.text] = currentValue.id;
+        return accumulator }, new Object()
+      );*/
+    await Promise.all(allSuggestionIds.map(suggestion => suggestion.textLater));
+    allSuggestionIds = allSuggestionIds.map(suggestion => {
+      return {id: suggestion.id, text: suggestion.textLater}
+    });
+    console.log(allSuggestionIds);
+    assert.ok(allSuggestionIds.length>0, allSuggestionIds);
+    console.log(allSuggestionIds);
+    let findSfRestaurant = allSuggestionIds.reduce((accumulator, currentValue) => {
+        if (accumulator) {
+          return accumulator;
+        }
+        return currentValue.text.startsWith('Find re') ? currentValue : null
+      }, null);
+    console.log(findSfRestaurant);
+    assert.ok(findSfRestaurant);
+    browser.elementIdClick(findRestaurants.id);
+    
+
+    //console.log('magnifier', magnifier);
+    //magnifier.click();
     // console.log('XXX result', result);
     // result.click();
     // browser.click(result);
