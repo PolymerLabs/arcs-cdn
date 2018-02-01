abf164c3613e6e39378d10e1f82bc0a5ae47393e
Scott J Miles <sjmiles@google.com>
2018-01-31 16:50:07 -0800

    undo bogus fix for dangling arc-handles

--- dev/app-shell/elements/arc-handle.js
+++ dev/app-shell/elements/arc-handle.js
@@ -59,10 +59,10 @@ class ArcHandle extends Xen.Base {
     ArcHandle.log('updating handle', handle.name, data);
     if (handle.toList) {
       data = Object.keys(data).map(key => {
-        return {id: handle.id, rawData: data[key]};
+        return {id: arc.generateID(), rawData: data[key]};
       });
     } else {
-      data = {id: handle.id, rawData: data};
+      data = {id: arc.generateID(), rawData: data};
     }
     ArcsUtils.setHandleData(handle, data);
   }
