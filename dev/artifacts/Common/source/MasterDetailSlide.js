0287685731bff5df8b851d44bf878371e968393b
Scott J Miles <sjmiles@google.com>
2018-02-02 15:28:31 -0800

    try to make the sliding panel less janky

--- dev/artifacts/Common/source/MasterDetailSlide.js
+++ dev/artifacts/Common/source/MasterDetailSlide.js
@@ -8,11 +8,11 @@
 
 "use strict";
 
-defineParticle(({DomParticle}) => {
+defineParticle(({DomParticle, html, log}) => {
 
   let host = `master-detail`;
 
-  let template = `
+  let template = html`
 <style>
   [${host}] {
     position: relative;
@@ -24,25 +24,28 @@ defineParticle(({DomParticle}) => {
     position: sticky;
     top: 64px;
     height: 0;
-    transform: translate3d(0, 100vh, 0);
-    transition: all 200ms ease-out;
     box-sizing: border-box;
-  }
-  [${host}] [detail-panel][open] {
-    transform: translate3d(0, 0, 0);
   }
   [${host}] [abs-panel] {
     position: absolute;
-    top: 0;
+    top: 56px;
     right: 0;
     left: 0;
-    height: calc(100vh - 64px);
+    height: calc(100vh - 32px);
     border-radius: 16px;
     padding: 0 16px;
     box-sizing: border-box;
     background-color: white;
     box-shadow: 0px 0px 6px 2px rgba(252,252,252,0.65);
     overflow-y: auto;
+    transform: translate3d(0, 100vh, 0);
+    transition: transform 100ms ease-out;
+  }
+  [${host}] [abs-panel][open] {
+    transform: translate3d(0, 0, 0);
+  }
+  [${host}] [abs-panel][hide] {
+    height: 0;
   }
   [${host}] button {
     background-color: transparent;
@@ -50,11 +53,13 @@ defineParticle(({DomParticle}) => {
   }
 </style>
 <div ${host}>
-  <!-- CSS tricks: zero-height position:sticky panel can autosize horizontally while not scrolling and not
-       pushing siblings out of position. Contained absolute panel can have vertical size without affecting
-       outer flow. -->
-  <div detail-panel open$="{{open}}" empty$="{{empty}}">
-    <div abs-panel>
+  <!--
+    CSS tricks: zero-height position:sticky panel can autosize horizontally while not scrolling and not
+    pushing siblings out of position. Contained absolute panel can have vertical size without affecting
+    outer flow.
+  -->
+  <div detail-panel>
+    <div abs-panel open$="{{open}}" hide$="{{hide}}">
       <div style="padding: 8px; text-align: right;"><button on-click="_onBack">X</button></div>
       <div slotid="detail"></div>
     </div>
@@ -69,25 +74,25 @@ defineParticle(({DomParticle}) => {
     get template() {
       return template;
     }
-    _render({selected}, {back}) {
-      let hasSelection = selected && (selected.name || selected.id);
-      if (!hasSelection) {
-        this._setState({back: false});
+    _render({selected}, state) {
+      let hide = true;
+      const open = Boolean(selected && (selected.name || selected.id));
+      if (open || state.open) {
+        // we are or were open, so don't hide right away
+        hide = false;
       }
-      return {
-        empty: !hasSelection,
-        open: Boolean(hasSelection && !back)
-      };
+      if (!open && state.open) {
+        // about to close, wait for animation before hiding
+        // state.open will be false next update (note: an update may occur before the timeout)
+        setTimeout(() => this._setState(), 400);
+      }
+      // record new open state
+      state.open = open;
+      return {hide, open};
     }
     _onBack() {
-      // trigger animation
-      this._setState({back: true});
-      // wait for animation to complete
-      setTimeout(() => {
-        // remove selection
-        this._views.get('selected').clear();
-        this._setState({back: false});
-      }, 300);
+      // remove selection
+      this._views.get('selected').clear();
     }
   };
 
