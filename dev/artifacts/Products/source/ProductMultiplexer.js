d6e3081f95b3269b29748f0400ee309788033764
Matt Small <mbsmall@google.com>
2018-02-02 09:39:15 -0800

    in-progress update of ProductMultiplexer from PolymerLabs/arcs@f411dd8 Remove the no-longer-needed ProductMultipler2 source files (it now uses a single source file and is declared separately in ProductMultipler.manifest). Note that this is not currently working; it fails with an error:
      Error: {Variable, [object Object]} is not of type Entity

--- dev/artifacts/Products/source/ProductMultiplexer.js
+++ dev/artifacts/Products/source/ProductMultiplexer.js
@@ -7,64 +7,123 @@
  * subject to an additional IP rights grant found at
  * http://polymer.github.io/PATENTS.txt
  */
+'use strict';
 
-"use strict";
-
-defineParticle(({DomParticle}) => {
-  return class ProductMultiplexer extends DomParticle {
+defineParticle(({TransformationDomParticle}) => {
+  return class ProductMultiplexer extends TransformationDomParticle {
     constructor() {
       super();
-      this._handleIds = new Set();
+      this._itemSubIdByHostedSlotId = new Map();
+      this._connByHostedConn = new Map();
     }
     async setViews(views) {
       let arc = await this.constructInnerArc();
-      this.on(views, 'products', 'change', async e => {
-        var productsView = views.get('products');
-        var productsList = await productsView.toList();
 
-        if (productsList.length > 0) {
+      let hostedParticle = await views.get('hostedParticle').get();
+
+      // Map all additional connections.
+      let otherMappedViews = [];
+      let otherConnections = [];
+      let index = 2;
+      for (let [connectionName, otherView] of views) {
+        if (['list', 'hostedParticle'].includes(connectionName)) {
+          continue;
+        }
+        otherMappedViews.push(`map '${await arc.mapHandle(otherView._view)}' as v${index}`);
+        let hostedOtherConnectionName = hostedParticle.connections.find(conn => conn.type.equals(otherView.type)).name;
+        otherConnections.push(`${hostedOtherConnectionName} <- v${index++}`);
+        this._connByHostedConn.set(hostedOtherConnectionName, connectionName);
+      }
+
+      this.on(views, 'list', 'change', async e => {
+        let handleIds = new Set(this._state.renderModel && this._state.renderModel.items ? this._state.renderModel.items.map(item => item.subId) : []);
+
+        let listHandle = views.get('list');
+        let list = await listHandle.toList();
+
+        if (list.length > 0) {
           this.relevance = 0.1;
         }
 
-        let hostedParticle = await views.get('hostedParticle').get();
-        for (let [index, product] of productsList.entries()) {
-          if (this._handleIds.has(product.id)) {
+        for (let [index, item] of list.entries()) {
+          if (handleIds.has(item.id)) {
             continue;
           }
-          let productView = await arc.createHandle(productsView.type.primitiveType(), "product" + index);
-          this._handleIds.add(product.id);
+          let itemView = await arc.createHandle(listHandle.type.primitiveType(), 'item' + index);
 
-          let hostedSlotName =  [...hostedParticle.slots.keys()][0];
+          let hostedSlotName = [...hostedParticle.slots.keys()][0];
           let slotName = [...this.spec.slots.values()][0].name;
           let slotId = await arc.createSlot(this, slotName, hostedParticle.name, hostedSlotName);
           if (!slotId) {
             continue;
           }
 
-          this.hostedSlotBySlotId.set(slotId, {subId: product.id});
+          this._itemSubIdByHostedSlotId.set(slotId, item.id);
 
-          let manifestFilePath = hostedParticle.implFile.replace(/\.[^\.]+$/, ".manifest").replace(/\/[^\/]+$/, "/..$&");
-          var recipe = `
-            import '${manifestFilePath}'
-            recipe
-              use '${productView._id}' as v1
-              slot '${slotId}' as s1
-              ${hostedParticle.name}
-                ${hostedParticle.connections[0].name} <- v1
-                consume ${hostedSlotName} as s1
-          `;
 
+          let recipe = `
+${this.serializeSchema(hostedParticle)}
+recipe
+  use '${itemView._id}' as v1
+  ${otherMappedViews.join('\n')}
+  slot '${slotId}' as s1
+  ${hostedParticle.name}
+    ${hostedParticle.connections[0].name} <- v1
+    ${otherConnections.join('\n')}
+    consume ${hostedSlotName} as s1
+`;
           try {
             await arc.loadRecipe(recipe, this);
-            productView.set(product);
+            itemView.set(item);
           } catch (e) {
             console.log(e);
           }
         }
+
+        // Update props of all particle's views.
+        this._updateAllViews(views, this.config);
       });
     }
-    _shouldRender(props) {
-      return false;
+
+    _willReceiveProps(props) {
+      let newItems = TransformationDomParticle.propsToItems(props.list);
+      // Join with existing items in particle's state.
+      if (this._state.renderModel && this._state.renderModel.items) {
+        newItems.forEach(newItem => {
+          let item = this._state.renderModel.items.find(item => item.subId == newItem.subId);
+          if (!!item) {
+            return Object.assign(newItem, item);
+          }
+        });
+      }
+      this._setState({renderModel: {items: newItems}});
+    }
+
+    combineHostedModel(slotName, hostedSlotId, content) {
+      let subId = this._itemSubIdByHostedSlotId.get(hostedSlotId);
+      if (!subId) {
+        return;
+      }
+      let items = this._state.renderModel ? this._state.renderModel.items : [];
+      let listIndex = items.findIndex(item => item.subId == subId);
+      if (listIndex >= 0 && listIndex < items.length) {
+        items[listIndex] = Object.assign({}, content.model, items[listIndex]);
+      } else {
+        items.push(Object.assign(content.model, {subId}));
+      }
+      this._setState({renderModel: {items}});
+    }
+
+    combineHostedTemplate(slotName, hostedSlotId, content) {
+      if (!this._state.template && !!content.template) {
+        let template = content.template;
+        // Replace hosted particle connection in template with the corresponding this particle connection names.
+        // TODO: make this generic!
+        this._connByHostedConn.forEach((conn, hostedConn) => {
+          template = template.replace(new RegExp(`{{${hostedConn}.description}}`, 'g'), `{{${conn}.description}}`);
+        });
+        this._setState({template});
+      }
     }
   };
 });
